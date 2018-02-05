3dbacf0fbafdedd7f2f55be9884fd7223f9ce222
Daniel Biedma <info@dbr.io>
2018-02-05 12:13:36 +0800

    feat(chore): make better interfaces and specs

---
+++ src/Interfaces.ts
@@ -0,0 +1,19 @@
+export type MethodInterface = {
+    method: string
+    dataFields: string[]
+    attributesFields: string[]
+    requiredFields: string[]
+}
+
+export type ConfigInterface = {
+    username: string
+    password: string
+    privateKeyPath?: string
+    privateKey?: string
+    environment?: string
+    endpoint?: string
+    publicKeyPath?: string
+    specs?: {
+        [key: string]: MethodInterface
+    }
+}
